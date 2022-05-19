72e5ae55340ea7c41c57f0783cae8d5f6c6b4404
Rob England <rob.eng@unb.ca>
2022-05-19 16:39:04 -0300

    theme menu now dynamically generates using js

--- scripts/theme-menu.js
+++ scripts/theme-menu.js
@@ -1,3 +1,58 @@
+const themeNames = ["light", "dark", "corroded", "arctic", "tropical", "neptune", "rust", "ugly", "intern"];
+
+buildThemeMenuOptions(themeNames);
+
+// Building the theme menu
+function buildThemeMenuOptions(themeNames){
+    const themeMenuOptions = document.querySelector("div#theme-menu-options");
+    
+    for(const themeName of themeNames){
+        const themeMenuOption = buildThemeMenuOption(themeName);
+        themeMenuOptions.appendChild(themeMenuOption);
+    }
+}
+
+function buildThemeMenuOption(themeName){
+    const themeMenuOption = document.createElement("div");
+    themeMenuOption.className = "theme-menu-option";
+
+    const themeMenuOptionInner = document.createElement("div");
+    themeMenuOptionInner.className = "theme-menu-option-inner";
+    themeMenuOption.appendChild(themeMenuOptionInner);
+
+    const themeMenuOptionPreview = buildThemeMenuOptionPreview(themeName);
+    const themeMenuOptionName = document.createElement("span");
+    themeMenuOptionName.className = "theme-menu-option-name";
+    themeMenuOptionName.textContent = capitalize(themeName);
+
+    if(themeName === "light"){
+        themeMenuOptionName.textContent += ` (default)`;
+    }
+
+    themeMenuOptionInner.appendChild(themeMenuOptionPreview);
+    themeMenuOptionInner.appendChild(themeMenuOptionName);
+
+    return themeMenuOption;
+}
+
+function buildThemeMenuOptionPreview(themeName){
+    const themeMenuOptionPreview = document.createElement("span");
+    themeMenuOptionPreview.className = `theme-menu-option-preview ${themeName}-preview`;
+
+    for(let i = 1; i < 4; i++){
+        const section = document.createElement("div");
+        section.className = "theme-menu-option-preview-section";
+        section.id = `${themeName}-sec-${i}`;
+        themeMenuOptionPreview.appendChild(section);
+    }
+
+    return themeMenuOptionPreview;
+}
+
+function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // Hide/show functionality of menu
 const themeMenuButton = document.querySelector("#theme-menu-button");
 const themeMenuOptions = document.querySelector("#theme-menu-options");
