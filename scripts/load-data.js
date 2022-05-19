a74ae2ebd957a29263dd1638182534e53a29f52f
Rob England <rob.eng@unb.ca>
2022-05-19 12:35:45 -0300

    entries now load from entries.json

--- scripts/load-data.js
+++ scripts/load-data.js
@@ -18,27 +18,130 @@ async function populateDocument(requestURLs, loadedContent){
     populateMain(loadedContent[0]);
 }
 
-function populateMain(entries){
-    console.log(entries);
-    for(const entry of entries){
-        appendEntry(entry);
+function populateMain(entriesData){
+    console.log(entriesData);
+    for(const entryData of entriesData){
+        appendEntry(entryData);
     }
 }
 
-function appendEntry(entry){
-    const articleID = entry["entryArticle"];
+function appendEntry(entryData){
+    const articleID = entryData["entryArticle"];
     articleContent = main.querySelector(`article#${articleID} > div.article-content`);
-    buildEntry(articleContent, entry);
+
+    const entry = buildEntry(entryData, articleID);
+    articleContent.appendChild(entry);
 }
 
-function buildEntry(articleContent, entry){
-    entryTitle = document.createElement("h4");
+function buildEntry(entryData, articleID){
+    const entry = document.createElement("div");
+    entry.className = "entry";
+
+    const entryTitle = buildEntryTitle(entryData)
+    entry.appendChild(entryTitle);
+
+    if(hasSubtitle(entryData)){
+        const entrySubtitle = document.createElement("p");
+        entrySubtitle.className = "entry-subtitle";
+        entry.appendChild(entrySubtitle);
+    }
+
+    if(hasLocation(entryData)){
+        const entryHR = document.createElement("hr");
+        entryHR.className = "entry-hr";
+        entry.appendChild(entryHR);
+    }
+
+    const entryDesc = buildEntryDesc(entryData, "fa-solid fa-angle-right", articleID);
+    entry.appendChild(entryDesc);
+
+    return entry;
+}
+
+function buildEntryTitle(entryData){
+    const entryTitle = document.createElement("h4");
     entryTitle.className = "entry-title";
-    console.log(entryTitle);
-    buildEntryTitle(articleContent, entryTitle, entry);
+    
+    const entryName = entryData["entryTitle"]["entryName"];
+    const entryNameSpan = document.createElement("span");
+    entryNameSpan.className = "entry-name";
+    entryNameSpan.textContent = entryName;
+    entryTitle.appendChild(entryNameSpan);
+    
+    if(hasLocation(entryData)){
+        const entryLocation = entryData["entryTitle"]["entryLocation"];
+        const entryLocationSpan = document.createElement("span");
+        entryLocationSpan.className = "entry-location";
+        entryLocationSpan.textContent = `${entryLocation["city"]}, ${entryLocation["province"]}`;
+        entryTitle.appendChild(entryLocationSpan);
+    }
+
+    if(hasPeriod(entryData)){
+        const entryPeriod = entryData["entryTitle"]["entryPeriod"];
+        const entryPeriodSpan = document.createElement("span");
+        entryPeriodSpan.className = "entry-period";
+        entryPeriodSpan.textContent = `${entryPeriod["periodStart"]} - ${entryPeriod["periodEnd"]}`;
+        entryTitle.appendChild(entryPeriodSpan);
+    }
+
+    return entryTitle;
 }
 
-function buildEntryTitle(articleContent, entryTitle, entry){
-    entryName = document.createElement("span");
-    entryName.className = "entry-name";
+function buildEntryDesc(entryData, faBulletIconClass, articleID){
+    const entryDesc = document.createElement("ul");
+    entryDesc.className = "entry-desc fa-ul";
+
+
+    if(articleID != "programming-skills"){
+        for(entryPoint of entryData["entryDesc"]){
+            const entryDescItem = buildEntryDescItem(faBulletIconClass, entryPoint);
+            entryDesc.appendChild(entryDescItem);
+        }
+    }
+    else{
+        let entryDescText = "";
+
+        for(let i = 0; i < entryData["entryDesc"].length; i++){
+            if((i+1) != entryData["entryDesc"].length){
+                entryDescText += `${entryData["entryDesc"][i]}, `;
+            }
+            else{
+                entryDescText += entryData["entryDesc"][i];
+            }
+        }
+
+        const entryDescItem = buildEntryDescItem(faBulletIconClass, entryDescText);
+        entryDesc.appendChild(entryDescItem);
+    }
+    return entryDesc;
+}
+
+function buildEntryDescItem(faBulletIconClass, text){
+    const entryDescItem = document.createElement("li");
+
+    const faSpan = document.createElement("span");
+    faSpan.className = "fa-li";
+    
+    const faBulletIcon = document.createElement("i");
+    faBulletIcon.className = faBulletIconClass;
+    
+    faSpan.appendChild(faBulletIcon);
+    entryDescItem.appendChild(faSpan);
+
+    const entryDescItemText = document.createTextNode(text);
+    entryDescItem.appendChild(entryDescItemText);
+
+    return entryDescItem;
+}
+
+function hasLocation(entryData){
+    return entryData["entryTitle"]["entryLocation"]["city"];
+}
+
+function hasPeriod(entryData){
+    return entryData["entryTitle"]["entryPeriod"]["periodStart"];
+}
+
+function hasSubtitle(entryData){
+    return entryData["entrySubtitle"];
 }
