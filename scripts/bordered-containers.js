9c37104ad910543a3867b5f43c2f77f0022cc805
Rob England <rob.eng@unb.ca>
2022-05-20 18:07:11 -0300

    Change styling

--- scripts/bordered-containers.js
+++ scripts/bordered-containers.js
@@ -2,17 +2,24 @@ borderedLinks = document.querySelectorAll(".bordered-link");
 borderedHeadings = document.querySelectorAll(".bordered-heading");
 
 styleBorderedLinks(borderedLinks);
-console.log("hi");
-console.log(borderedLinks);
+styleBorderedHeadings(borderedHeadings);
 
 function styleBorderedLinks(borderedLinks){
     for(const borderedLink of borderedLinks){
-        console.log(`link: ${borderedLink}`);
-        let containerWidth = borderedLink.clientWidth;
-        console.log(containerWidth);
+        const containerHeight = borderedLink.offsetHeight;
+        const borderWidth = 0.04*containerHeight
+        const thickBorderWidth = 0.15*containerHeight;
+
+        borderedLink.style.borderWidth = `${borderWidth}px ${thickBorderWidth}px ${thickBorderWidth}px ${borderWidth}px`;
     }
 }
 
 function styleBorderedHeadings(borderedHeadings){
+    for(const borderedHeading of borderedHeadings){
+        const containerHeight = borderedHeading.offsetHeight;
+        const borderWidth = 0*containerHeight;
+        const thickBorderWidth = 0.12*containerHeight;
 
+        borderedHeading.style.borderWidth = `${borderWidth}px ${borderWidth}px ${borderWidth}px ${thickBorderWidth}px`;
+    }
 }
